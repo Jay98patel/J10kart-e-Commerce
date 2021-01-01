e8f1b146e63823c58c4eb99b07e0ca4ac6f18149
Jay98patel <itsj10.jey@gmail.com>
2021-01-01 22:03:37 +0530

    navbar, component creation & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { UserOrdersComponent } from './components/user-orders/user-orders.component';
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'orders',component:UserOrdersComponent},
+  {path:'cart',component:CartComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
